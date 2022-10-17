a522f183362d71365ea6866a9b07f8da89ce7b99
victormerch <1Victormerch@gmail.com>
2022-10-17 20:21:36 +0200

    filtros de try and except acabados pero quedan tareas por acabar

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 """
 ======TAREAS======
-- [ ] Implementar try and catch para los posibles errores, MEDIO Hecho
-      Pedir datos de la base de datos y comprobar que estos se pongan bien por teclado
+- [ ] Comprobar que el fichero sea .csv
+- [ ] Mirar de hacer diferente el que se cree siempre una tabla nueva, 
+      y el poner opcion de insertar en una tabla ya existente o crear una nueva
 - [ ] Comentar codigo
 ==================
 """
@@ -10,35 +11,58 @@ from lectorCSV import LectorCSV
 from sqlConector import SQLConector
 
 def main():
-    print("=====Lector de CSV y cargador a MySQL=====")
+    flag = True  
     lcsv = LectorCSV()
     sql = SQLConector()
+    
+    print("==========================================")  
+    print("=====Lector de CSV y cargador a MySQL=====")
+    print("==========================================")
     try:
-        fileName = input("-Introduce el nombre del fichero -> ")
-
-        # Comprobamos que el fichero existe
-        if not lcsv.leerfichero(fileName+".csv"):
-            raise ValueError ("El fichero donde estan ubicados los datos no existe")
-        print("Procesando informacion del csv...")
         
+        print("\n====Introduzca los datos de la base de datos====")
         #Preguntar datos de la base de datos
+        user = input("-Introduce el usuario de la base de datos -> ")
+        password = input("-Introduce la contraseña de la base de datos -> ")
+        host = input("-Introduce el host de la base de datos -> ")
+        nombreBD = input("-Introduce el nombre de la base de datos -> ")
         
-        if not sql.conexion("root","test","localhost","IABD"):
+        if not sql.conexion(user,password,host,nombreBD):
             raise ValueError ("No se ha podido conectar a la base de datos")
         
-        print("Base de datos conectada...")
+        print("===Base de datos conectada===\n")
         
-        columnas = lcsv.getColumns()
-        typeColumns = lcsv.getcolumsTypes()
-        datos = lcsv.getRows()
-        print("Insertando datos en la base de datos...")
         
-        name = input("Que nombre quieres darle a la tabla? ")
-        if sql.createTable(name,typeColumns,columnas,datos):
-            print("Datos insertados correctamente")
+        while flag:
+        # Comprobamos que el fichero existe
+            fileName = input("-Introduce el nombre del fichero .csv -> ")
             
-        else:
-            print("Error en la conexion")
+            if not lcsv.leerfichero(fileName+".csv"):
+                raise ValueError ("El fichero donde estan ubicados los datos no existe")
+            print("Procesando informacion del csv...")
+            
+            columnas = lcsv.getColumns()
+            typeColumns = lcsv.getcolumsTypes()
+            datos = lcsv.getRows()
+            
+            
+            name = input("-Que nombre quieres darle a la tabla? ")
+            if  not sql.createTable(name,typeColumns,columnas,datos):
+                raise ValueError ("No se ha podido insertar los datos en la base de datos")
+            
+            print("===Datos insertados correctamente===\n")
+            
+            while True:
+                option = input("-Quieres insertar mas datos? (s/n) -> ")
+                if option=="n" or option=="N":
+                    flag = False
+                    break
+                elif option=="s" or option=="S":
+                    break
+                else:
+                    print("Error, opcion no valida")
+            
+            
         
             
     except ValueError as Error:#Captador de errores
--- sqlConector.py
+++ sqlConector.py
@@ -12,34 +12,57 @@ class SQLConector:
         self.db = db
         try:
             self.mydb = mysql.connector.connect(host=self.host,
-                                            user=self.user,
-                                            password=self.password,
-                                            database=self.db)
+                                                user=self.user,
+                                                password=self.password,
+                                                database=self.db)
                                         
             return True
         except mysql.connector.Error as err:
             print("Error: {}".format(err))
             return False
-    def createTable(self,name,columsTypes,columns,datos):
     
-        query1 = f"CREATE TABLE {name} ({columns[0]} {columsTypes[0]} PRIMARY KEY,{columns[1]} {columsTypes[1]},{columns[2]} {columsTypes[2]}, {columns[3]} {columsTypes[3]},{columns[4]} {columsTypes[4]},{columns[5]} {columsTypes[5]},{columns[6]} {columsTypes[6]},{columns[7]} {columsTypes[7]},{columns[8]} {columsTypes[8]},{columns[9]} {columsTypes[9]},{columns[10]} {columsTypes[10]},{columns[11]} {columsTypes[11]})"
-        query2 = f"insert into {name} values "
-        for i in range (0,len(datos[columns[0]])):
-            #falta la conversion de tipos de datos
-            #['BIGINT', 'BIGINT', 'BIGINT', 'VARCHAR(100)', 'VARCHAR(100)', 'FLOAT', 'BIGINT', 'BIGINT', 'VARCHAR(100)', 'FLOAT', 'VARCHAR(100)', 'VARCHAR(100)']
-           query2 +=  f"({datos[columns[0]][i]},{datos[columns[1]][i]},{datos[columns[2]][i]},'{datos[columns[3]][i]}', '{datos[columns[4]][i]}',{datos[columns[5]][i]},{datos[columns[6]][i]},{datos[columns[7]][i]},'{datos[columns[8]][i]}', {datos[columns[9]][i]},'{datos[columns[10]][i]}','{datos[columns[11]][i]}')"
-           if i != len(datos[columns[0]])-1:
-               query2 += ","
-        
-        mycursor = self.mydb.cursor()
-        mycursor.execute(query1)
-        
-        
-        mycursor.execute(query2)
-        self.mydb.commit()
-        
-        
-        return True
+    def createTable(self,name,columsTypes,columns,datos):
+        try:
+            query1 = f"CREATE TABLE {name} ("
+            for i in range(len(columns)):
+                
+                    
+                query1 += f"{columns[i]} {columsTypes[i]}"
+                if i == 0:
+                    query1 += " PRIMARY KEY"
+                if i != len(columns)-1:
+                    query1 += ","
+                else:
+                    query1 += ");"
+                    
+            query2 = f"insert into {name} values "
+            for i in range (0,len(datos[columns[0]])):
+                row = "("
+                
+                for y in range (0,len(columns)):
+                    if columsTypes[y] == "VARCHAR(100)":
+                        row += f"'{datos[columns[y]][i]}'"
+                    else:
+                        row += f"{datos[columns[y]][i]}"
+                    if y != len(columns)-1:
+                        row += ","
+                query2 += row + ")"
+                
+                if i != len(datos[columns[0]])-1:
+                    query2 += ","
+                else:
+                    query2 += ";"
+            
+            mycursor = self.mydb.cursor()
+            mycursor.execute(query1)
+            mycursor.execute(query2)
+            
+            self.mydb.commit()
+            
+            return True
+        except mysql.connector.Error as err:
+            print("Error: {}".format(err))
+            return False
                             
        
        
